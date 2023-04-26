bd90bf7d19c63290cd8ae98f7cec78fbec6ca980
KubaGarncarek <kubagarncarek47@gmail.com>
2023-04-26 14:04:25 +0200

    update analysis in R

--- R_files/data_exploration.R
+++ R_files/data_exploration.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lubridate)
 
 # remove continents data
 
@@ -27,6 +28,106 @@ case_per_test <- country_total_cases %>%
 
 
 case_per_test %>% filter(case_per_test >= 0.05)
+
+
+
+vaccinations_data %>% group_by(date) %>%
+  summarize(vac = sum(new_vaccinations)) %>%
+  ggplot(aes(date, vac))+
+  geom_point()
+
+# percent people vaccinated
+
+vac_percent <- vaccinations_data %>% group_by(location) %>%
+  summarize(vac = sum(new_vaccinations), population=population[1])
+
+sum(vac_percent$vac)/sum(vac_percent$population)
+
+# percent vaccinated people in last 6 months
+
+vac_percent <- vaccinations_data %>%
+  filter(today() - vaccinations_data$date <360) %>%
+  group_by(location) %>%
+  summarize(vac = sum(new_vaccinations), population=population[1])
+
+sum(vac_percent$vac)/sum(vac_percent$population)
+
+
+# max stringency in country
+
+vaccinations_data %>%
+  group_by(location) %>% filter(stringency_index >0) %>%
+  summarize(str = max(stringency_index))
+
+
+# mean stringency in country
+
+vaccinations_data %>%
+  group_by(location) %>%
+  summarize(str = mean(stringency_index, na.rm=TRUE)) %>% print(n=200)
+
+# population density
+
+dens <- vaccinations_data %>% group_by(location) %>%
+  summarize(dens = population_density[1])
+
+plot(dens$dens)
+
+
+# percent of older people
+
+sixty_five <- vaccinations_data %>% group_by(location) %>%
+  summarize(sixty_five = aged_65_older[1])
+
+sixty_five %>% 
+  ggplot(aes(sixty_five))+
+  geom_histogram()
+
+seventy <- vaccinations_data %>% group_by(location) %>%
+  summarize(seventy = aged_70_older[1])
+
+seventy %>% 
+  ggplot(aes(seventy))+
+  geom_histogram()
+
+# gdp
+
+gdp <- vaccinations_data %>% group_by(location) %>%
+  summarize(gdp = gdp_per_capita[1])
+
+gdp %>%
+  ggplot(aes(gdp)) +
+  geom_histogram()
+
+
+# cardiovascular death rate
+
+cardiovascular_death_rate <- vaccinations_data %>% group_by(location) %>% 
+  summarize(card = cardiovasc_death_rate[1])
+
+cardiovascular_death_rate %>%
+  ggplot(aes(card)) +
+  geom_histogram()
+
+
+# diabetes rate
+
+diabetes <- vaccinations_data %>% group_by(location) %>%
+  summarize(diabetes = diabetes_prevalence[1])
+
+diabetes %>%
+  ggplot(aes(diabetes))+
+  geom_histogram()
+
+
+# life expectancy 
+
+life_expectancy <- vaccinations_data %>% group_by(location) %>%
+  summarize(exp = life_expectancy[1])
+
+life_expectancy %>%
+  ggplot(aes(exp)) +
+  geom_histogram()
 
 
 
